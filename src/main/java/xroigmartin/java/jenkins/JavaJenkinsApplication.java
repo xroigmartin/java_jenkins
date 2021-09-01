0f3304b90b221834acf8008014617f047fc5068c
Xavier Roig <xroigmartin@gmail.com>
2021-09-01 23:21:08 +0200

    initial commit

---
+++ src/main/java/xroigmartin/java/jenkins/JavaJenkinsApplication.java
@@ -0,0 +1,13 @@
+package xroigmartin.java.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xroigmartin/java/jenkins/JavaJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package xroigmartin.java.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
